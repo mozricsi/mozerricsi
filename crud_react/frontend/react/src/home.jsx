4e7e389017d7047b840164f15b1e0fd75bee92da
mozricsi <mozric721@hengersor.hu>
2025-01-31 13:23:50 +0100

    react

---
+++ crud_react/frontend/react/src/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+ 
+const Home=()=>{
+    return(
+        <div>
+            <div>
+                <div>
+                    <h1>Hello world!</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
