14cd5a1093a287ad37cdb2c73ca101152c68fabf
mozricsi <mozric721@hengersor.hu>
2025-02-26 13:22:02 +0100

    oraimunka

---
+++ backend/fogado/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <h1>Panzió</h1>
+
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
